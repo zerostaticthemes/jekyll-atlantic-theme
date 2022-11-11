1479de4eb9c1f8a24367807f0cc7abad5284a1d9
Klaudia Jusko <klaudia.jusko@student.uj.edu.pl>
2022-11-11 18:51:23 +0100

    extending colors pallete test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,29 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ["./_includes/**/*.html", "./_layouts/**/*.html", "./blog/*.html", "./_posts/*.html", "./*.html"],
-  darkMode: false,
-  theme: {
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
+    purge: ["./_includes/**/*.html", "./_layouts/**/*.html", "./blog/*.html", "./_posts/*.html", "./*.html"],
+    darkMode: false,
+    theme: {
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+        },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            orange: '#e9a881',
+            amber: colors.amber
+        }
     },
-  },
-  variants: {
-    extend: {
-      grayscale: ["hover", "focus"],
-      margin: ["last"],
+    variants: {
+        extend: {
+            grayscale: ["hover", "focus"],
+            margin: ["last"],
+        },
+        container: [],
     },
-    container: [],
-  },
-  plugins: [require("@tailwindcss/typography")],
+    plugins: [require("@tailwindcss/typography")],
 };
